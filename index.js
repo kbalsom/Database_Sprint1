842168b4b982d6a5c52b4eaf664e0d5248949bf8
kbalsom <100210446+kbalsom@users.noreply.github.com>
2022-10-25 23:27:35 -0230

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/cities', db.getCities)
+app.get('/city', db.getCityById)
+app.get('/cities/search', db.findCitiesByName)
+
+app.get('/airports', db.getAirports)
+app.get('/airport', db.getAirportById)
+app.get('/airports/search/name', db.findAirportsByName)
+app.get('/airports/search/code', db.findAirportsByCode)
+
+app.get('/aircrafts', db.getAircrafts)
+app.get('/aircraft', db.getAircraftById)
+
+app.get('/passengers', db.getPassengers)
+app.get('/passenger', db.getPassengerById)
+
+app.get('/airportscities', db.getAirportsCities)
+app.get('/aircraftsairports', db.getAircraftsAirports)
+app.get('/passengersaircrafts', db.getPassengersAircrafts)
+app.get('/passengersairports', db.getPassengersAirports)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
+
